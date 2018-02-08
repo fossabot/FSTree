a7c996dbdcccb42df5e127fdf65548a168fe2413
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-02-08 13:35:03 +0530

    Alpha version

---
+++ fileListScript.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import configparser
+import os
+import subprocess
+
+# Instantiating the configparser to read the 'config.ini'
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Fetching the sections list and storing the name of the first section
+
+sectionsList = config.sections()
+dirsSection = sectionsList[0]
+print(sectionsList)
+print(dirsSection)
+
+# Traversing the list of directories and printing the tree of each directory
+
+for key in config[dirsSection]:
+	print(key)
+	dir = config[dirsSection][key]
+	print(dir)
+	os.chdir(dir)
+	print(os.getcwd())
+
+	cmd = ['tree','-alpuhD','--du']
+	returned_output = subprocess.check_output(cmd)
+	print(returned_output.decode('utf-8'))
