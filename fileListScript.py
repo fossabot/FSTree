f3f40f9533dd805705515b13a736e6b17ce9c8c1
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-02-12 10:52:45 +0530

    Using directory name for filename

--- fileListScript.py
+++ fileListScript.py
@@ -18,15 +18,15 @@ dirsSection = sectionsList[0]
 # Traversing the list of directories and printing the tree of each directory
 
 for key in config[dirsSection]:
-	dir = config[dirsSection][key]
-	os.chdir(dir)
+	directory_path = config[dirsSection][key]
+	os.chdir(directory_path)
 
 	cmd = ['tree','-alpuhD','--du']
 	returned_output = subprocess.check_output(cmd).decode('utf-8')
 
 	date = datetime.datetime.now().strftime("%d_%b_%y_%a")
-
-	filename = "{}_{}.txt".format(key, date)
+	directory_name = os.path.split(directory_path)[-1]
+	filename = "{}_{}.txt".format(directory_name, date)
 	print("Filename: " + filename)
 
 	file = open(filename, mode='w', encoding='utf-8')
