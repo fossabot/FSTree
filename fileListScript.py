5863be076688d78126bc420dd8cd316013ed1e77
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-02-12 09:24:39 +0530

    Improved filename for brevity

--- fileListScript.py
+++ fileListScript.py
@@ -3,6 +3,7 @@
 import configparser
 import os
 import subprocess
+import datetime
 
 # Instantiating the configparser to read the 'config.ini'
 
@@ -13,22 +14,21 @@ config.read('config.ini')
 
 sectionsList = config.sections()
 dirsSection = sectionsList[0]
-print(sectionsList)
-print(dirsSection)
 
 # Traversing the list of directories and printing the tree of each directory
 
 for key in config[dirsSection]:
-	print(key)
 	dir = config[dirsSection][key]
-	print(dir)
 	os.chdir(dir)
-	print(os.getcwd())
 
 	cmd = ['tree','-alpuhD','--du']
 	returned_output = subprocess.check_output(cmd).decode('utf-8')
 
-	filename = "tree.txt"
+	date = datetime.datetime.now().strftime("%d_%b_%y_%a")
+
+	filename = "{}_{}.txt".format(key, date)
+	print("Filename: " + filename)
+
 	file = open(filename, mode='w', encoding='utf-8')
 	print(returned_output, file=file)
 	file.close()
